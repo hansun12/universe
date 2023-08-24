afe45490788c40e3c83691a0f18f5ab34c6d4799
hansun12 <songhs-0601@naver.com>
2023-08-24 18:18:10 +0900

    :tada:3학기 프로젝트 정보 초기화

--- CSE214/TestCse214.java
+++
@@ -1,8 +0,0 @@
-package universe.CSE214;
-
-public class TestCse214 {
-   public static void main(String[] args) {
-        System.out.println("This is CSE 214");
-        System.out.println("Trying github");
-   }
-}
